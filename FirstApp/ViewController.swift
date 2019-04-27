c1a3f22135b689c027af19f2203bb0ca6c446ad4
kyoichiro <kyoichiro.kobayashi@ams.eng.osaka-u.ac.jp>
2019-04-27 16:56:33 +0900

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by admin on 2019/04/27.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
